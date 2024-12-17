0a3f44d2a0ba277e0083e9d2c21eaef2e9bd46ca
Administrator Regula <administratorregula@xcode.regula.local>
2024-12-17 13:25:29 +0300

    Release 7.5.10925

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["Bounds"]),
     ],
     targets: [
-        .binaryTarget(name: "Bounds", url: "https://pods.regulaforensics.com/Bounds/7.4.9820/DocumentReaderCore_bounds_7.4.9820.zip", checksum: "61ab5fa45ba8f7ab207c1c142a3b2336913add52f56185ff7e22d43ffcff2316"),
+        .binaryTarget(name: "Bounds", url: "https://pods.regulaforensics.com/Bounds/7.5.10925/DocumentReaderCore_bounds_7.5.10925.zip", checksum: "45df8b7fa3535bae8f750dbd72980d1a47220fbfb88deef2d0459222653ca11a"),
     ]
 )
