0be6cf941ff445410762f7ef8f3e7e090c914c69
Jenkins <jenkins@regula.by>
2026-06-29 22:14:54 +0300

    Release 9.6.18851

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["Bounds"]),
     ],
     targets: [
-        .binaryTarget(name: "Bounds", url: "https://pods.regulaforensics.com/Bounds/9.5.18259/DocumentReaderCore_bounds_9.5.18259.zip", checksum: "374ef6388c9acd2b2086a55d71a1b17bbe0a0e2f25186c833978e7694ee695ec"),
+        .binaryTarget(name: "Bounds", url: "https://pods.regulaforensics.com/Bounds/9.6.18851/DocumentReaderCore_bounds_9.6.18851.zip", checksum: "d231649d68bfd0a8b20dd55ca47b11ef7a0770436547d193237685f501476f4a"),
     ]
 )
